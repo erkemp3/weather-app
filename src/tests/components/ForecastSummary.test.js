25f47a9b180afa1789f1acf74269ad909957468d
Ellie Kemp <elliekemp95@gmail.com>
2021-06-17 18:04:48 +0100

    Forecast Summary Snapshot test added and passed

--- src/tests/components/ForecastSummary.test.js
+++ src/tests/components/ForecastSummary.test.js
@@ -3,5 +3,25 @@ import { render } from "@testing-library/react";
 import ForecastSummary from "../../components/ForecastSummary";
 
 describe("ForecastSummary", () => {
-  it("renders a div with correct classname", () => {});
+  const validProps = {
+    date: 1111111,
+    description: "Stub description",
+    icon: "stubIcon",
+    temperature: {
+      min: 12,
+      max: 22,
+    },
+  };
+
+  it("renders correctly", () => {
+    const { asFragment } = render(
+      <ForecastSummary
+        date={validProps.date}
+        description={validProps.description}
+        icon={validProps.icon}
+        temperature={validProps.temperature}
+      />
+    );
+    expect(asFragment()).toMatchSnapshot();
+  });
 });
