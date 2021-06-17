5a1a04afe43f9c54b8b6ba96643629dafe01e03b
Ellie Kemp <elliekemp95@gmail.com>
2021-06-17 19:11:38 +0100

    Forecast Summaries component added and rendered in the DOM

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 import LocationDetails from "./LocationDetails";
+import ForecastSummaries from "./ForecastSummaries";
 import "../styles/App.css";
 
-const App = (props) => {
-  const { location } = props;
-  return (
-    <div className="weather-app">
-      <LocationDetails city={location.city} country={location.country} />
-    </div>
-  );
-};
+const App = ({ location, forecasts }) => (
+  <div className="forecast">
+    <LocationDetails city={location.city} country={location.country} />
+
+    <ForecastSummaries forecasts={forecasts} />
+  </div>
+);
 
 App.propTypes = {
+  forecasts: PropTypes.arrayOf(
+    PropTypes.shape({
+      date: PropTypes.number,
+      description: PropTypes.string,
+      icon: PropTypes.number,
+      temperature: PropTypes.shape({
+        max: PropTypes.number,
+        min: PropTypes.number,
+      }),
+    })
+  ).isRequired,
   location: PropTypes.shape({
     city: PropTypes.string,
     country: PropTypes.string,
